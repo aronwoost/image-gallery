44f1f31746a78e7a70353e26fd46c7dde32ccca5
Aron Woost <aron@springworks.de>
2024-01-15 15:07:20 +0100

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Test',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <button type='button' >Open Gallery</button>
+      </div>
+    </main>
+  )
+}

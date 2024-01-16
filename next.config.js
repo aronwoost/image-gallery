ec4a19a1a41185b25ff8e8f41a32d35e8d6ff2eb
Aron Woost <aron@springworks.de>
2024-01-16 14:35:30 +0100

    Fix eslint

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const commit = require('child_process')
   .execSync('git rev-parse --short HEAD')
   .toString()

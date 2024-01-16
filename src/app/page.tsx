19117b7c7df3112ecb78484ead8d3056a10847b8
Aron Woost <aron@springworks.de>
2024-01-16 13:12:48 +0100

    Remove ImageGalleryContainer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { useState } from "react";
-import ImageGalleryContainer from "./ImageGalleryContainer";
+import ImageGallery from "./ImageGallery";
+
+import styles from "./page.module.css";
+
+const IMAGES = [
+  "./image02.jpg",
+  "./image03.jpg",
+  "./image04.jpg",
+  "./image05.jpg",
+  "./image06.jpg",
+];
 
 export default function Home() {
   const [galleryOpen, setGalleryOpen] = useState(false);
@@ -14,7 +24,13 @@ export default function Home() {
         </button>
       </div>
       {galleryOpen && (
-        <ImageGalleryContainer onClose={() => setGalleryOpen(false)} />
+        <div className={styles.container}>
+          <ImageGallery
+            index={0}
+            images={IMAGES}
+            onClose={() => setGalleryOpen(false)}
+          />
+        </div>
       )}
     </main>
   );

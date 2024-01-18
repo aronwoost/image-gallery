7f0cb6bcfd3687a827872188b6670440204af710
Aron Woost <aron@springworks.de>
2024-01-18 12:08:37 +0100

    Implement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,16 @@ import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
-  title: 'Test',
+  title: 'Image Gallery',
+  description: '',
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+  maximumScale: 1,
+  userScalable: 'no',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
